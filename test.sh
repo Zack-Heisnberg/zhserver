aff8d6b0bb740e70e46022a5dbed88740a77620a
Zack-Heisnberg <69718435+Zack-Heisnberg@users.noreply.github.com>
2021-02-12 15:27:07 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-apt-get install -y tzdata
-dpkg-reconfigure --frontend noninteractive tzdata
-su --preserve-environment
-apt-get install lxde-core tightvncserver -y
+export DEBIAN_FRONTEND=noninteractive &&
+apt-get install -y locales &&
+locale-gen en_US.UTF-8 &&
+ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime &&
+apt-get install -y tzdata &&
+dpkg-reconfigure --frontend noninteractive tzdata &&
+su --preserve-environment &&
+apt-get install lxde-core tightvncserver -y &&
 apt-get install -y wget gnupg ca-certificates curl zip sudo wget \
      && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - \
      && sudo apt-get install -y nodejs
-mkdir node
-cd node
-npm install express ngrok node-persist
-myuser="root"
-mypasswd="mysecret"
-mkdir /home/$myuser/.vnc
-echo $mypasswd | vncpasswd -f > /home/$myuser/.vnc/passwd
-chown -R $myuser:$myuser /home/$myuser/.vnc
-chmod 0600 /home/$myuser/.vnc/passwd
-npm install -g pm2
-wget -qO /home/$myuser/.vnc/xstartup https://github.com/Zack-Heisnberg/zhserver/raw/master/xstartup
+mkdir /root/.vnc &
+mkdir node &&
+cd node &&
+npm install express ngrok node-persist &&
+wget -qO app.js https://github.com/Zack-Heisnberg/zhserver/raw/master/app.js &&
+npm install -g pm2 &&
+pm2 start app.js &&
+echo $passwd | vncpasswd -f > /root/.vnc/passwd &&
+chown -R root:root /root/.vnc &&
+chmod 0600 /root/.vnc/passwd &&
+wget -qO /root/.vnc/ https://github.com/Zack-Heisnberg/zhserver/raw/master/xstartup &&
 tightvncserver :1
